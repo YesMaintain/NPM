814557f5ccd9974ecd7ed707903d4fbab55fce3e
Nikola Hristov <nikola@nikolahristov.tech>
2022-11-10 08:09:39 +0200

    squash!

--- scripts/dist/lib/gits.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * It returns a set of all the git repositories in the current directory
- */
-declare const gits: () => Promise<Set<string>>;
-export default gits;
--- scripts/src/lib/request.ts
+++ scripts/src/lib/request.ts
@@ -1,5 +1,6 @@
 import { Octokit } from "@octokit/core";
 import type { OctokitResponse } from "@octokit/types";
+import { deepmerge } from "deepmerge-ts";
 
 import env from "../lib/env.js";
 
@@ -16,8 +17,17 @@ const request = async (
 		console.log(`Successfully ${where}`);
 
 		switch (type) {
-			case "octokit":
-				return await octokit.request(where, _with);
+			case "octokit": {
+				return await octokit.request(
+					where,
+					deepmerge(_with, {
+						headers: {
+							"If-None-Match":
+								"19a5941476078432824fadaeb04143d60602c3e00dac483c4ddc2f7af86c1e75",
+						},
+					})
+				);
+			}
 		}
 	} catch (e) {
 		console.log(`Could not ${where}`);
