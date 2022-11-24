f0fd4bf201503d581ec854d59fb11499c0c8a2c2
Nikola Hristov <nikola@nikolahristov.tech>
2022-11-24 04:08:56 +0200

    squash!

--- scripts/dist/command/dependabot.js
+++ scripts/dist/command/dependabot.js
@@ -1,7 +1,7 @@
-import*as e from"fs";import{dirname as l}from"path";import w from"./../lib/git-directories.js";import $ from"./../lib/package-types.js";import d from"./../lib/packages.js";import g from"./../options/dependabot.js";const y=async p=>{for(const{path:o,name:r,workflow:f}of p)for(const[s,m]of await w(await d())){const t=`${s}/.github`,a=await f();if(o==="/")for(const i of m){const c=l(i).replace(s,""),n=(await $()).get(i.split("/").pop());a.add(`
-    - package-ecosystem: "${typeof n<"u"?n:(()=>{switch(i.split(".").pop()){case"csproj":return"nuget";default:return"npm"}})()}"
-      directory: "${c||"/"}"
+import*as t from"fs";import{dirname as m}from"path";import d from"./../lib/git-directories.js";import w from"./../lib/package-types.js";import $ from"./../lib/packages.js";import u from"./../options/dependabot.js";const y=async p=>{for(const{path:o,name:r,workflow:f}of p)for(const[c,l]of await d(await $())){const e=`${c}/.github`,s=await f();if(o==="/")for(const a of l){const n=m(a).replace(c,""),i=(await w()).get(a.split("/").pop());s.add(`
+    - package-ecosystem: "${typeof i<"u"?i:(()=>{switch(a.split(".").pop()){case"csproj":return"nuget";default:return"npm"}})()}"
+      directory: "${n||"/"}"
       schedule:
           interval: "daily"
-      versioning-strategy: increase
-`)}if(a.size>0){try{await e.promises.mkdir(`${t}${o}`,{recursive:!0})}catch{console.log(`Could not create: ${t}${o}`)}try{await e.promises.writeFile(`${t}${o}${r}`,`${[...a].join("")}`)}catch{console.log(`Could not create workflow for: ${t}/dependabot.yml`)}}else try{await e.promises.access(`${t}${o}${r}`,e.constants.F_OK);try{await e.promises.rm(`${t}${o}${r}`)}catch{console.log(`Could not remove ${o}${r} for: ${t}`)}}catch{}}};var j=async()=>{await y(g)};export{j as default};
+      versioning-strategy: ${typeof i<"u"?i:(()=>{switch(a.split(".").pop()){case"cargo":return"lockfile-only";default:return"increase"}})()}
+`)}if(s.size>0){try{await t.promises.mkdir(`${e}${o}`,{recursive:!0})}catch{console.log(`Could not create: ${e}${o}`)}try{await t.promises.writeFile(`${e}${o}${r}`,`${[...s].join("")}`)}catch{console.log(`Could not create workflow for: ${e}/dependabot.yml`)}}else try{await t.promises.access(`${e}${o}${r}`,t.constants.F_OK);try{await t.promises.rm(`${e}${o}${r}`)}catch{console.log(`Could not remove ${o}${r} for: ${e}`)}}catch{}}};var j=async()=>{await y(u)};export{j as default};
