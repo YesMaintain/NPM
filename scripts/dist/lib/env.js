89b864891bbcc42ebf0066001ff3129f855b8276
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-14 00:42:38 +0200

    squash!

--- scripts/dist/lib/env.js
+++
@@ -1,5 +0,0 @@
-import r from "dotenv";
-import { cwd as o } from "process";
-import { z as t } from "zod";
-r.config();var d=t.object({GITHUB_USER:t.string().default(""),BASE_DIR:t.string().default(o()),GITHUB_AUTH_TOKEN:t.string().default("")}).parse(process.env);export { d as default };
-
