279dba7120f21f268ea22e34e8a15d955fd5cd7d
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-22 14:00:58 +0300

    squash!

--- Script/Source/Command/Cloudflare.ts
+++ Script/Source/Command/Cloudflare.ts
@@ -1,10 +1,15 @@
 import { constants as Constant } from "fs";
-import { access, mkdir, readFile, rm, writeFile } from "fs/promises";
-import { dirname as Dir } from "path";
+import {
+	access as Access,
+	writeFile as _File,
+	mkdir as Make,
+	rm as Remove,
+} from "fs/promises";
+import { basename, dirname } from "path";
 import gitDirectories from "../Library/Directory.js";
-import Package from "../Library/Package.js";
-import Type from "../Library/Type.js";
-import Node from "../Option/Node.js";
+import Packages from "../Library/Package.js";
+import Types from "../Library/Type.js";
+import Rust from "../Option/Rust.js";
 import type { Files } from "../Option/Index.js";
 
 /**
@@ -12,133 +17,38 @@ import type { Files } from "../Option/Index.js";
  * it checks if the file is a node workflow file, and if it is, it checks if the file is a node
  * workflow file for a package that has dependencies, and if it is, it adds the dependencies to the
  * workflow file
- * @param {Files} files - containers
+ * @param {Files} Files - containers
  */
-const Workflow = async (files: Files) => {
-	for (const { Path, Name, File } of files) {
+const Workflow = async (Files: Files) => {
+	for (const { Path, Name, File } of Files) {
 		for (const [directory, packageFiles] of await gitDirectories(
-			await Package("Cloudflare")
+			await Packages("Cloudflare")
 		)) {
 			const githubDir = `${directory}/.github`;
 			const workflowBase = await File();
 
 			if (Path === "/workflows/" && Name === "Cloudflare.yml") {
 				for (const _package of packageFiles) {
-					const packageDirectory = Dir(_package).replace(
+					const packageDirectory = dirname(_package).replace(
 						directory,
 						""
 					);
-					const packageFile = (
-						await readFile(_package, "utf-8")
-					).toString();
 
-					const environment = (await Type()).get(
+					const environment = (await Types()).get(
 						_package.split("/").pop()
 					);
 
 					if (
 						typeof environment !== "undefined" &&
-						environment === "npm"
+						environment === "Cloudflare"
 					) {
-						try {
-							const packageJSON = JSON.parse(packageFile);
-
-							for (const bundle of [
-								"bundledDependencies",
-								"bundleDependencies",
-								"dependencies",
-								"devDependencies",
-								"extensionDependencies",
-								"optionalDependencies",
-								"peerDependencies",
-								"peerDependenciesMeta",
-							].sort()) {
-								if (
-									typeof packageJSON[bundle] !== "undefined"
-								) {
-									workflowBase.add(`
-            - uses: actions/setup-node@v3.8.1
-              with:
-                  node-version: \${{ matrix.node-version }}
-                  cache: "pnpm"
-                  cache-dependency-path: .${packageDirectory}/pnpm-lock.yaml
-
-            - run: pnpm install
-              working-directory: .${packageDirectory}
-`);
-								}
-							}
-
-							for (const key in packageJSON) {
-								if (
-									Object.prototype.hasOwnProperty.call(
-										packageJSON,
-										key
-									)
-								) {
-									const values = packageJSON[key];
-									if (key === "scripts") {
-										for (const scripts in values) {
-											if (
-												Object.prototype.hasOwnProperty.call(
-													values,
-													scripts
-												)
-											) {
-												if (scripts === "build") {
-													workflowBase.add(`
-            - run: pnpm run build
-              working-directory: .
-
-            - uses: actions/upload-artifact@v3.1.2
-              with:
-                  name: .${packageDirectory.replaceAll(
-						"/",
-						"-"
-					)}-Node-\${{ matrix.node-version }}-Target
-                  path: .${packageDirectory}/Target
-`);
-												}
-
-												if (
-													scripts === "prepublishOnly"
-												) {
-													workflowBase.add(`
-            - run: pnpm run prepublishOnly
-              working-directory: .
-
-            - uses: actions/upload-artifact@v3.1.2
-              with:
-                  name: .${packageDirectory.replaceAll(
-						"/",
-						"-"
-					)}-Node-\${{ matrix.node-version }}-Target
-                  path: .${packageDirectory}/Target
-`);
-												}
-
-												if (scripts === "test") {
-													workflowBase.add(`
-            - run: pnpm run test
-              working-directory: .${packageDirectory}
-`);
-												}
-											}
-										}
-									}
-								}
-							}
-						} catch (_Error) {
-							console.log(_package);
-							console.log(_Error);
-						}
 					}
 				}
 			}
 
 			if (workflowBase.size > 1) {
 				try {
-					await mkdir(`${githubDir}${Path}`, {
+					await Make(`${githubDir}${Path}`, {
 						recursive: true,
 					});
 				} catch {
@@ -146,21 +56,21 @@ const Workflow = async (files: Files) => {
 				}
 
 				try {
-					await writeFile(
+					await _File(
 						`${githubDir}${Path}${Name}`,
 						`${[...workflowBase].join("")}`
 					);
 				} catch {
 					console.log(
-						`Could not create workflow for: ${githubDir}/workflows/Node.yml`
+						`Could not create workflow for: ${githubDir}/workflows/Cloudflare.yml`
 					);
 				}
 			} else {
 				try {
-					await access(`${githubDir}${Path}${Name}`, Constant.F_OK);
+					await Access(`${githubDir}${Path}${Name}`, Constant.F_OK);
 
 					try {
-						await rm(`${githubDir}${Path}${Name}`);
+						await Remove(`${githubDir}${Path}${Name}`);
 					} catch {
 						console.log(
 							`Could not remove ${Path}${Name} for: ${githubDir}`
@@ -172,4 +82,4 @@ const Workflow = async (files: Files) => {
 	}
 };
 
-export default async () => await Workflow(Node);
+export default async () => await Workflow(Rust);
