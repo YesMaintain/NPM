0bc8e8472cf24e879d295ced1bd3ee4231111986
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-11 19:48:39 +0300

    squash!

--- Script/Target/Command/NPM.js
+++ Script/Target/Command/NPM.js
@@ -1,10 +1,10 @@
-import{constants as g}from"fs";import{access as u,mkdir as d,readFile as k,rm as h,writeFile as N}from"fs/promises";import{dirname as O}from"path";import P from"../Library/Directory.js";import F from"../Library/Package.js";import b from"../Library/Type.js";import v from"../Option/NPM.js";const M=async m=>{for(const{Path:o,Name:e,File:w}of m)for(const[n,y]of await P(await F("NPM"))){const t=`${n}/.github`,i=await w();if(o==="/workflows/"&&e==="NPM.yml")for(const a of y){const s=O(a).replace(n,""),$=(await k(a,"utf-8")).toString(),l=(await b()).get(a.split("/").pop());if(typeof l<"u"&&l==="NPM")try{const r=JSON.parse($);for(const c in r)if(Object.prototype.hasOwnProperty.call(r,c)){const p=r[c];if(c==="scripts")for(const f in p)Object.prototype.hasOwnProperty.call(p,f)&&f==="prepublishOnly"&&i.add(`
-            - name: Publish .${s}
+import{constants as g}from"fs";import{access as u,mkdir as d,rm as k,readFile as h,writeFile as N}from"fs/promises";import{dirname as O}from"path";import P from"../Library/Directory.js";import F from"../Library/Package.js";import _ from"../Library/Type.js";import b from"../Option/NPM.js";const v=async m=>{for(const{Path:o,Name:t,File:w}of m)for(const[c,y]of await P(await F("NPM"))){const e=`${c}/.github`,i=await w();if(o==="/workflows/"&&t==="NPM.yml")for(const a of y){const n=O(a).replace(c,""),$=(await h(a,"utf-8")).toString(),l=(await _()).get(a.split("/").pop());if(typeof l<"u"&&l==="NPM")try{const r=JSON.parse($);for(const s in r)if(Object.prototype.hasOwnProperty.call(r,s)){const p=r[s];if(s==="scripts")for(const f in p)Object.prototype.hasOwnProperty.call(p,f)&&f==="prepublishOnly"&&i.add(`
+            - name: Publish .${n}
               continue-on-error: true
-              working-directory: .${s}
+              working-directory: .${n}
               run: |
                   npm install --legacy-peer-deps
                   npm publish --legacy-peer-deps --provenance
               env:
                   NODE_AUTH_TOKEN: \${{ secrets.NPM_TOKEN }}
-`)}}catch(r){console.log(a),console.log(r)}}if(i.size>1){try{await d(`${t}${o}`,{recursive:!0})}catch{console.log(`Could not create: ${t}${o}`)}try{await N(`${t}${o}${e}`,`${[...i].join("")}`)}catch{console.log(`Could not create workflow for: ${t}/workflows/NPM.yml`)}}else try{await u(`${t}${o}${e}`,g.F_OK);try{await h(`${t}${o}${e}`)}catch{console.log(`Could not remove ${o}${e} for: ${t}`)}}catch{}}};var S=async()=>await M(v);export{S as default};
+`)}}catch(r){console.log(a),console.log(r)}}if(i.size>1){try{await d(`${e}${o}`,{recursive:!0})}catch{console.log(`Could not create: ${e}${o}`)}try{await N(`${e}${o}${t}`,`${[...i].join("")}`)}catch{console.log(`Could not create workflow for: ${e}/workflows/NPM.yml`)}}else try{await u(`${e}${o}${t}`,g.F_OK);try{await k(`${e}${o}${t}`)}catch{console.log(`Could not remove ${o}${t} for: ${e}`)}}catch{}}};var S=async()=>await v(b);export{S as default};
--- Script/Target/Command/Rust.js
+++ Script/Target/Command/Rust.js
@@ -1,4 +1,4 @@
-import{constants as f}from"fs";import{access as g,writeFile as $,mkdir as p,rm as w}from"fs/promises";import{basename as u,dirname as d}from"path";import y from"../Library/Directory.js";import h from"../Library/Package.js";import k from"../Library/Type.js";import F from"../Option/Rust.js";const C=async n=>{for(const{Path:o,Name:a,File:l}of n)for(const[s,m]of await y(await h("Cargo"))){const t=`${s}/.github`,e=await l();if(o==="/workflows/"&&a==="Rust.yml")for(const r of m){const i=d(r).replace(s,""),c=(await k()).get(r.split("/").pop());typeof c<"u"&&c==="Cargo"&&e.add(`
+import{constants as f}from"fs";import{access as g,writeFile as $,mkdir as p,rm as w}from"fs/promises";import{basename as u,dirname as d}from"path";import y from"../Library/Directory.js";import h from"../Library/Package.js";import k from"../Library/Type.js";import F from"../Option/Rust.js";const C=async n=>{for(const{Path:o,Name:a,File:l}of n)for(const[s,m]of await y(await h("Cargo"))){const t=`${s}/.github`,r=await l();if(o==="/workflows/"&&a==="Rust.yml")for(const e of m){const i=d(e).replace(s,""),c=(await k()).get(e.split("/").pop());typeof c<"u"&&c==="Cargo"&&r.add(`
             - uses: actions/cache@v3.3.1
               with:
                   path: |
@@ -12,5 +12,5 @@ import{constants as f}from"fs";import{access as g,writeFile as $,mkdir as p,rm a
             - uses: actions-rs/cargo@v1.0.3
               with:
                 command: build
-                args: --release --all-features --manifest-path .${i}/${u(r)}
-`)}if(e.size>1){try{await p(`${t}${o}`,{recursive:!0})}catch{console.log(`Could not create: ${t}${o}`)}try{await $(`${t}${o}${a}`,`${[...e].join("")}`)}catch{console.log(`Could not create workflow for: ${t}/workflows/Rust.yml`)}}else try{await g(`${t}${o}${a}`,f.F_OK);try{await w(`${t}${o}${a}`)}catch{console.log(`Could not remove ${o}${a} for: ${t}`)}}catch{}}};var O=async()=>await C(F);export{O as default};
+                args: --release --all-features --manifest-path .${i}/${u(e)}
+`)}if(r.size>1){try{await p(`${t}${o}`,{recursive:!0})}catch{console.log(`Could not create: ${t}${o}`)}try{await $(`${t}${o}${a}`,`${[...r].join("")}`)}catch{console.log(`Could not create workflow for: ${t}/workflows/Rust.yml`)}}else try{await g(`${t}${o}${a}`,f.F_OK);try{await w(`${t}${o}${a}`)}catch{console.log(`Could not remove ${o}${a} for: ${t}`)}}catch{}}};var O=async()=>await C(F);export{O as default};
