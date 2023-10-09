13c56a4d92233f13b83e547499784a8f7701b0c8
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-09 04:57:59 +0300

    squash!

--- Script/Configuration/ESBuild.js
+++ Script/Configuration/ESBuild.js
@@ -1,35 +1,39 @@
 import { copy as Copy } from "esbuild-plugin-copy";
-import { access as Access, constants as Constant, mkdir as Make, rm as Remove, } from "fs/promises";
+import {
+	access as Access,
+	constants as Constant,
+	mkdir as Make,
+	rm as Remove,
+} from "fs/promises";
 const Out = "Target";
 export default {
-    format: "esm",
-    minify: false,
-    outdir: Out,
-    platform: "node",
-    target: "esnext",
-    write: true,
-    plugins: [
-        {
-            name: "Target",
-            setup(Build) {
-                Build.onStart(async () => {
-                    try {
-                        await Remove(Out, {
-                            recursive: true,
-                        });
-                    }
-                    catch (_Error) { }
-                });
-            },
-        },
-        Copy({
-            resolveFrom: "out",
-            assets: [
-                {
-                    from: "./Source/Workflow/*.yml",
-                    to: "./Workflow/",
-                },
-            ],
-        }),
-    ],
+	format: "esm",
+	minify: false,
+	outdir: Out,
+	platform: "node",
+	target: "esnext",
+	write: true,
+	plugins: [
+		{
+			name: "Target",
+			setup(Build) {
+				Build.onStart(async () => {
+					try {
+						await Remove(Out, {
+							recursive: true,
+						});
+					} catch (_Error) {}
+				});
+			},
+		},
+		Copy({
+			resolveFrom: "out",
+			assets: [
+				{
+					from: "./Source/Workflow/*.yml",
+					to: "./Workflow/",
+				},
+			],
+		}),
+	],
 };
